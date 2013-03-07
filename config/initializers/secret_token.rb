27cf3511dc52edec91cc9a2cb9e5ec223226c5ef
Desmond <moermus@gmail.com>
2013-03-07 02:37:33 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RecycleApp::Application.config.secret_token = 'fb6b782d3fdde2ec8522c1630943d38b7083bd0024afe65c5ad2d6f87fb380834dd1f7a22a956035a82711dc54ea905201acf976b7f73023a52f97cd9c749ece'
